3b5e5e9960683708d917da8f8f5dbc0c828053a5
riltonbispo <riltondev@gmail.com>
2024-01-28 14:45:58 -0300

    feat: about page

---
+++ pages/about/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Content = styled.section`
+  width: 90%;
+  max-width: 25.5rem;
+  margin: 2rem auto;
+
+  & * {
+    margin-bottom: 1rem;
+    line-height: 120%;
+  }
+
+  & h3 {
+    font-weight: 600;
+    font-size: 1.125rem;
+  }
+
+  & p {
+    font-size: 0.875rem;
+  }
+`
