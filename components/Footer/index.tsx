b81dde022f11e1e57fd4558f80b8755f6dc481a7
riltonbispo <riltondev@gmail.com>
2024-01-28 12:06:21 -0300

    feat: footer

---
+++ components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './style'
+const Footer = () => {
+  return (
+    <S.Footer>
+      Todas as marcas e ilustrações utilizadas são de seus resepctivos donos.
+    </S.Footer>
+  )
+}
+
+export default Footer
---
+++ components/Footer/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { theme } from "../../styles/theme";
+
+export const Footer = styled.footer`
+  background-color: ${theme.darkColor};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat; 
+  color: white;
+  text-align: center;
+  padding-block: 1.5rem;
+  width: 100vw;
+`
